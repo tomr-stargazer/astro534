c81297eed15e985ea8d464d5f87a7cfa130db2d7
Tom Rice <t.rice90@gmail.com>
2015-01-28 20:14:05 -0500

    convert tabs to spaces

--- hw1_2c.py
+++ hw1_2c.py
@@ -14,44 +14,44 @@ import astropy.units as u
 import astropy.constants as c
 
 def hubble_time(redshift, hubble_constant):
-	""" 
-	Returns Hubble time at redshift z using hubble constant H_0.
+    """ 
+    Returns Hubble time at redshift z using hubble constant H_0.
 
-	Assumes H_0 given in km s^-1 Mpc^-1.
+    Assumes H_0 given in km s^-1 Mpc^-1.
 
-	"""
+    """
 
-	z = redshift
-	H_0 = u.Quantity(hubble_constant, u.km / u.s / u.Mpc)
+    z = redshift
+    H_0 = u.Quantity(hubble_constant, u.km / u.s / u.Mpc)
 
-	t_H = 2/(3*H_0) * (1 + z)**(-3/2)
+    t_H = 2/(3*H_0) * (1 + z)**(-3/2)
 
-	return t_H.to('Gyr')
+    return t_H.to('Gyr')
 
 def lookback_time(redshift, hubble_constant):
 
-	t_H_0 = hubble_time(0, hubble_constant)
-	t_H_z = hubble_time(redshift, hubble_constant)
+    t_H_0 = hubble_time(0, hubble_constant)
+    t_H_z = hubble_time(redshift, hubble_constant)
 
-	return t_H_0 - t_H_z
+    return t_H_0 - t_H_z
 
 
 
 def calculate_values_for_2c(redshift=2, list_of_H0s=[50,70,100]):
 
-	z = redshift
+    z = redshift
 
-	print "Hubble Time at z={0}".format(z)
-	for H_0 in list_of_H0s:
+    print "Hubble Time at z={0}".format(z)
+    for H_0 in list_of_H0s:
 
-		print "    H_0={0} km s^-1 Mpc^-1:".format(H_0)
-		print "   {0:.2f}".format(hubble_time(z, H_0))
+        print "    H_0={0} km s^-1 Mpc^-1:".format(H_0)
+        print "   {0:.2f}".format(hubble_time(z, H_0))
 
-	print "Lookback Time at z={0}".format(z)
-	for H_0 in list_of_H0s:
+    print "Lookback Time at z={0}".format(z)
+    for H_0 in list_of_H0s:
 
-		print "    H_0={0} km s^-1 Mpc^-1:".format(H_0)
-		print "   {0:.2f}".format(lookback_time(z, H_0))
+        print "    H_0={0} km s^-1 Mpc^-1:".format(H_0)
+        print "   {0:.2f}".format(lookback_time(z, H_0))
 
 
 
