64feea72d1bd2a41756737cb113a13c2d2fd0f55
Tom Rice <t.rice90@gmail.com>
2015-01-28 19:30:12 -0500

    Wrote a lot more for HW4! It's much more involved and better.

--- hw1_4.py
+++ hw1_4.py
@@ -17,18 +17,43 @@ import astropy.constants as c
 from scipy.integrate import quad
 
 
-def hubble_time_integrand(z, omega_matter, omega_lambda):
+def _hubble_time_inner_function(z, omega_matter, omega_lambda):
 
-	return 1 / ( (1+z) * (omega_matter*(1+z)**3 + omega_lambda)**(1/2) )
+    return 1 / ((1 + z) * (omega_matter * (1 + z)**3 + omega_lambda)**(1/2))
+
 
 def hubble_time_general(redshift, hubble_constant, omega_matter, omega_lambda):
-	"""
+    """
+    Returns Hubble time at redshift z using hubble constant H_0, omega_matter, omega_lambda.
 
-	"""
+    Assumes H_0 given in km s^-1 Mpc^-1.
 
-	z = redshift
-	H_0 = u.Quantity(hubble_constant, u.km / u.s / u.Mpc)
+    """
 
-	t_H_z = quad(hubble_time_integrand, z, np.inf)
+    z = redshift
+    H_0 = u.Quantity(hubble_constant, u.km / u.s / u.Mpc)
 
-	return t_H_z
+    hubble_time_integrand = lambda z: _hubble_time_inner_function(
+        z, omega_matter, omega_lambda)
+
+    t_H_z = 1 / H_0 * quad(hubble_time_integrand, z, np.inf)[0]
+
+    return t_H_z.to(u.Gyr)
+
+
+def _angular_diameter_inner_function(z, omega_matter, omega_lambda):
+
+	return 1 / (omega_matter * (1+z)**3 + omega_lambda)**(1/2)
+
+
+def angular_diameter_distance(redshift, hubble_constant, omega_matter, omega_lambda):
+
+    z = redshift
+    H_0 = u.Quantity(hubble_constant, u.km / u.s / u.Mpc)
+
+    angular_diameter_integrand = lambda z: _angular_diameter_inner_function(
+        z, omega_matter, omega_lambda)
+
+    d_A_z = c.c / H_0 * 1/(1+z) *  quad(angular_diameter_integrand, 0, z)[0]
+
+    return d_A_z.to(u.Gpc)
